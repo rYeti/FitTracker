d9e3d8bf58491704689d97674198320acd2df329
rYeti <Robert.Stertz1@HS-Augsburg.DE>
2025-06-30 23:10:45 +0200

    Refactor database connection setup for cross-platform support. Implement separate connection logic for web and native platforms, and update dependencies for sqlite3_flutter_libs.

--- lib/core/app_database.dart
+++ lib/core/app_database.dart
@@ -1,41 +1,16 @@
-import 'dart:io';
-import 'package:flutter/foundation.dart'; // For kIsWeb
 import 'package:drift/drift.dart';
-import 'package:drift/web.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:drift/native.dart';
-import 'package:path/path.dart' as p;
+import 'app_database_connection.dart'
+    if (dart.library.io) 'app_database_connection_native.dart'
+    if (dart.library.html) 'app_database_connection_web.dart';
 
 part 'app_database.g.dart';
 
 @DriftDatabase(tables: [FoodItem], daos: [FoodItemDao])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase() : super(_openConnection());
+  AppDatabase() : super(connect());
 
   @override
   int get schemaVersion => 1;
-}
-
-LazyDatabase _openConnection() {
-  if (kIsWeb) {
-    // Use IndexedDB for web
-    return LazyDatabase(() async {
-      return WebDatabase('fittracker');
-    });
-  } else if (Platform.isAndroid ||
-      Platform.isIOS ||
-      Platform.isLinux ||
-      Platform.isMacOS ||
-      Platform.isWindows) {
-    // Use NativeDatabase for other platforms
-    return LazyDatabase(() async {
-      final dir = await getApplicationDocumentsDirectory();
-      final file = File(p.join(dir.path, 'fittracker.sqlite'));
-      return NativeDatabase(file);
-    });
-  } else {
-    throw UnsupportedError('Unsupported platform');
-  }
 }
 
 class FoodItem extends Table {
@@ -45,7 +20,7 @@ class FoodItem extends Table {
   IntColumn get carbs => integer()();
   IntColumn get fat => integer()();
 
-  @override
+  @overridex
   Set<Column> get primaryKey => {name};
 }
 
